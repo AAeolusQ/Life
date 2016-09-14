7e2e698256aece676e50bebccc19e5f2278f4db7
AAeolusQ <qjj110821_love@163.com>
2016-09-14 11:05:44 +0800

    添加主页面

--- LifeProject/app/src/main/java/com/aeolus/lifeproject/LoadingActivity.java
+++ LifeProject/app/src/main/java/com/aeolus/lifeproject/LoadingActivity.java
@@ -1,13 +1,24 @@
 package com.aeolus.lifeproject;
 
+import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class LoadingActivity extends AppCompatActivity {
+public class LoadingActivity extends FragmentActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    Thread.sleep(3000);
+                } catch (InterruptedException e) {
+                }
+            }
+        }).start();
     }
 }
---
+++ LifeProject/app/src/main/java/com/aeolus/lifeproject/view/activity/HomeActivity.java
@@ -0,0 +1,19 @@
+package com.aeolus.lifeproject.view.activity;
+
+import com.aeolus.lifeproject.BaseActivity;
+import com.aeolus.lifeproject.R;
+
+import org.xutils.view.annotation.ContentView;
+
+/**
+ * 主页面
+ * Created by aeolus.qian on 2016/9/14.
+ */
+@ContentView(R.layout.activity_home)
+public class HomeActivity extends BaseActivity{
+
+    @Override
+    protected void bindView() {
+
+    }
+}
